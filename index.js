df191f4a5b2cdcd42981ae5d87f49f991da6d620
Ibrahim <ibkhalil5425@gmail.com>
2023-10-31 21:26:56 +0600

    "static dir add"

--- index.js
+++ index.js
@@ -7,14 +7,11 @@ const app = express()
 const port = process.env.PORT || 3000
 const path = require('path')
 
-
-// app.set("view engine", "ejs")
-// app.set("views", "views")
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.engine('ejs', require('ejs').renderFile);
 
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(middlewares)
 app.use(router)
   
